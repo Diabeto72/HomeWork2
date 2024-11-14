530a81587fdbb858afc8ffaf7092b68515ca35f0
Ido_A <idoamir100@gmail.com>
2024-11-15 00:05:34 +0200

    chaged _active to start as true

--- Q1_HW2/Device.cpp
+++ Q1_HW2/Device.cpp
@@ -1,8 +1,3 @@
-// Q1_HW2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-#include <string>
 #include "Device.h"
 
 void Device::init(unsigned int id, DeviceType type, std::string os)
@@ -10,6 +5,7 @@ void Device::init(unsigned int id, DeviceType type, std::string os)
     _id = id;
     _type = type;
     _os = os;
+    _active = true;  
 }
 
 std::string Device::getOS() const
@@ -17,12 +13,12 @@ std::string Device::getOS() const
     return _os;
 }
 
-unsigned int Device::getID()
+unsigned int Device::getID() const
 {
     return _id;
 }
 
-DeviceType Device::getType()
+DeviceType Device::getType() const
 {
     return _type;
 }
@@ -41,18 +37,3 @@ void Device::deactivate()
 {
     _active = false;
 }
-int main()
-{
-    
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
