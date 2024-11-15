6665a18d298a9c46893507c575189a99b0f3eb6f
Ido_A <idoamir100@gmail.com>
2024-11-15 23:40:42 +0200

    updated user.h

--- Q1_HW2/User.h
+++ Q1_HW2/User.h
@@ -1,3 +1,25 @@
 #pragma once
-void init(unsigned int id,
-	std::string username, unsigned int age)
+
+#include "DeviceList.h"
+#include <string.h>
+
+class User
+{
+public:
+	void init(unsigned int id, std::string username, unsigned int age);
+	void clear();
+	unsigned int getID() const;
+	std::string getUserName() const;
+	unsigned int getAge() const;
+	DevicesList& getDevices() const;
+	void addDevice(Device newDevice);
+	bool checkIfDevicesAreOn() const;
+private:
+	unsigned int _id;
+	std::string _username;
+	unsigned int _age;
+	Device _newDevice;
+
+};
+
+
